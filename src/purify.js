bc7fcc6c3d273c5b5e79d328ecac628df12bad91
Alejandro Leal <22180346+AstroCorp@users.noreply.github.com>
2022-11-08 11:10:42 +0100

    support for ALLOWED_URI_REGEXP

--- src/purify.js
+++ src/purify.js
@@ -1,18 +1,25 @@
 #!/usr/bin/env node
-const createDOMPurify = require('dompurify');
-const { JSDOM } = require('jsdom');
-const fs = require('fs');
+const createDOMPurify = require("dompurify");
+const { JSDOM } = require("jsdom");
+const fs = require("fs");
 
-const window = new JSDOM('').window;
+const window = new JSDOM("").window;
 const DOMPurify = createDOMPurify(window);
 
-
-
-let [dirtyPath, config] = process.argv.slice(2)
+let [dirtyPath, config] = process.argv.slice(2);
 
 const dirty = fs.readFileSync(dirtyPath);
-config = JSON.parse(Buffer.from(config, 'base64').toString('ascii'));
+config = JSON.parse(Buffer.from(config, "base64").toString("ascii"));
 
+if (config["ALLOWED_URI_REGEXP"]) {
+  const main = config["ALLOWED_URI_REGEXP"].match(/\/(.+)\/.*/)[1];
+  const options = config["ALLOWED_URI_REGEXP"].match(/\/.+\/(.*)/)[1];
+  const regex = new RegExp(main, options);
+
+  if (dirty.toString().search(regex) !== -1) {
+    config["ALLOWED_URI_REGEXP"] = regex;
+  }
+}
 
 const clean = DOMPurify.sanitize(dirty, config);
 
--- tests/Unit/PurifyTest.php
+++ tests/Unit/PurifyTest.php
@@ -10,6 +10,8 @@ class PurifyTest extends TestCase
     public function test_returns_sanitized_svg()
     {
         $process = new Purifier;
+
+        // Clean events
         $output = $process->clean('<svg><g onload="alert(\'test\')"></g>', [
             'USE_PROFILES' => [
                 'svg' => true
@@ -18,7 +20,7 @@ class PurifyTest extends TestCase
 
         $this->assertEquals('<svg><g></g></svg>', $output);
 
-
+        // Clean js
         $output = $process->clean('<svg><script>alert(\'test\')</script></svg>', [
             'USE_PROFILES' => [
                 'svg' => true
@@ -26,6 +28,16 @@ class PurifyTest extends TestCase
         ]);
 
         $this->assertEquals('<svg></svg>', $output);
+
+        // Regex
+        $output = $process->clean("<a href=\"https://www.wepik.com\" /><a href=\"https://www.wepik.es\" />", [
+            'USE_PROFILES' => [
+                'html' => true,
+            ],
+            'ALLOWED_URI_REGEXP' => "/https:\/\/www\.[a-z0-9-]+(?:\.[a-z0-9-]+)*\.com/g",
+        ]);
+
+        $this->assertEquals('<a href="https://www.wepik.com"></a><a></a>', $output);
     }
 
 }
